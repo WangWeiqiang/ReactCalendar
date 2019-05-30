87d8289bc95c2d014aef04ecc5fc963d3c825f18
WANG Weiqiang <wangwq_net@hotmail.com>
2019-05-30 18:20:06 +0800

    added year navigation

--- js/app.js
+++ js/app.js
@@ -1,72 +1,37 @@
+var calendarDataofCurrentYear={
+  year:2019,
+  monthNames : ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"],
+  weekNames : ["Su","M","Tu","W","Th","F","Sa"],
+  publicHolidays : {
+    '2019-1-1':'New Year\'s Day',
+    '2019-2-5':'Chinese New Year',
+    '2019-2-6':'Chinese New Year',
+    '2019-4-19':'Good Friday',
+    '2019-5-1':'Labour Day',
+    '2019-5-19':'Vesak Day',
+    '2019-6-5':'Hari Raya Puasa',
+    '2019-8-9':'National Day',
+    '2019-8-11':'Hari Raya Haji',
+    '2019-10-27':'Deepvali',
+    '2019-12-25':'Christmas Day'
+  },
+  birthDays : {
+    '2019-5-1':['Kelvin','Lucy']
+  },
 
-// class Calendar extends React.Component{
-//   render {
-//     return (
-//       <h>hellow world</h>
-//     )
-//   }
-// }
-const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"]
-const weekNames =["Su","M","Tu","W","Th","F","Sa"]
+  anniversary : {
+    '2019-7-2':'Wedding',
+    '2019-5-1':'Starhub'
+  },
 
-const publicHolidays ={
-  '2019-1-1':'New Year\'s Day',
-  '2019-2-5':'Chinese New Year',
-  '2019-2-6':'Chinese New Year',
-  '2019-4-19':'Good Friday',
-  '2019-5-1':'Labour Day',
-  '2019-5-19':'Vesak Day',
-  '2019-6-5':'Hari Raya Puasa',
-  '2019-8-9':'National Day',
-  '2019-8-11':'Hari Raya Haji',
-  '2019-10-27':'Deepvali',
-  '2019-12-25':'Christmas Day'
+  busyDays : [
+    '2019-6-25',
+    '2019-10-11',
+    '2019-5-1'  
+  ]
+
 }
-
-const birthDays ={
-  '2019-5-1':['Kelvin','Lucy']
-}
-
-const anniversary={
-  '2019-7-2':'Wedding',
-  '2019-5-1':'Starhub'
-}
-
-const busyDays=[
-  '2019-6-25',
-  '2019-10-11',
-  '2019-5-1'
-
-]
-
-
-const today=new Date()
 
 const rootElement = document.getElementById('root')
 
-const dates =[
-  {
-    month:'January',
-    days:[
-      [
-        null,
-        {
-          date:'2019-1-1',
-          weekDay:1,
-          publicHoliday:{name:'Chinese New Year'},
-          birthDay:{people:'WWQ'},
-          busy:true,
-          anniversary:{name:'wedding'}
-        }
-
-      ],
-    ]
-  }
-]
-
-  
-ReactDOM.render(
-  <Calendar monthNames={monthNames} weekNames={weekNames} 
-    publicHolidays={publicHolidays} birthDays={birthDays} 
-    busyDays={busyDays} anniversaries = {anniversary}/>,
-  rootElement)
+ReactDOM.render(<Calendar data={calendarDataofCurrentYear}/>, rootElement)
--- js/components/Calendar.js
+++ js/components/Calendar.js
@@ -1,4 +1,18 @@
 class Calendar extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            year:props.data.year
+        }
+        this.changeYear = this.changeYear.bind(this)
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        console.log(this.state.year)
+        console.log(nextState.year)
+        return this.state.year==nextState.year
+    }
+
     listToMatrix(list, elementsPerSubArray) {
         var matrix = [], i, k;
       
@@ -14,14 +28,12 @@ class Calendar extends React.Component{
         return matrix;
     }
 
-    
-
-    createMonths(monthNames){
+    createMonths(year,monthNames){
         
         const today=new Date()
         return monthNames.map(m => {
-            const firstDay = new Date(today.getFullYear(), monthNames.indexOf(m), 1);
-            const lastDay = new Date(today.getFullYear(), monthNames.indexOf(m) + 1, 0);
+            const firstDay = new Date(year, monthNames.indexOf(m), 1);
+            const lastDay = new Date(year, monthNames.indexOf(m) + 1, 0);
             var days=[]
             const weekDayOfFirstDay = firstDay.getDay()
             const weekDayOfLastDay = lastDay.getDay()
@@ -30,15 +42,15 @@ class Calendar extends React.Component{
             }
 
             for(var i=1;i<=lastDay.getDate();i++){
-                var date=new Date(today.getFullYear(),monthNames.indexOf(m),i)
+                var date=new Date(year,monthNames.indexOf(m),i)
                 var dateString = date.getFullYear()+'-'+ (date.getMonth()+1)+'-'+date.getDate()
 
                 days.push({
-                    date:new Date(today.getFullYear(),monthNames.indexOf(m),i),
-                    publichHoliday:this.props.publicHolidays[dateString],
-                    birthDays:this.props.birthDays[dateString],
-                    busy:this.props.busyDays.indexOf(dateString)>=0,
-                    anniversary:this.props.anniversaries[dateString]
+                    date:new Date(year,monthNames.indexOf(m),i),
+                    publichHoliday:this.props.data.publicHolidays[dateString],
+                    birthDays:this.props.data.birthDays[dateString],
+                    busy:this.props.data.busyDays.indexOf(dateString)>=0,
+                    anniversary:this.props.data.anniversary[dateString]
                 })
             }
             for(var i=weekDayOfLastDay;i<6;i++){
@@ -55,19 +67,53 @@ class Calendar extends React.Component{
 
     }
 
+    changeYear(direction){
+        var year = this.state.year+(direction=="left"?-1:1)
+        this.props.data.year=year
+        this.setState({year:year})
+    }
+
+    
     render(){
         
-        const months =  this.createMonths(this.props.monthNames)
+        const months =  this.createMonths(this.props.data.year,this.props.data.monthNames)
+
+        const ButtonChangeYear=({direction,onClick})=>{
+            return (
+                <button className="btn btn-sm btn-success" 
+                    onClick={event=>{
+                        if (!!onClick) {
+                            onClick(direction);
+                        }
+                    }}><i className={"fa fa-caret-"+(direction=="left"?"left":"right")}></i></button>
+            )
+        }
 
         return (
-
-                 <div className="row" key='month-list'>
+            <div>
+                <div className="row">
+                    <ButtonChangeYear direction="left" onClick={this.changeYear}></ButtonChangeYear>
+                    <ButtonChangeYear direction="right" onClick={this.changeYear}></ButtonChangeYear>
+                </div>
+                <div className="row">
+                    <div className="col-sm-5"><h4>Calendar for Year {this.props.data.year}</h4></div>
+                    <div className='col-sm-7 text-right'>
+                        <ul className="legend">
+                            <li><i className='fa fa-umbrella-beach text-success'></i>Public Holiday</li>
+                            <li><i className='fa fa-birthday-cake text-danger'></i>Birthday</li>
+                            <li><i className='fa fa-flag text-info'></i>Anniversary</li>
+                            <li><i className='fa fa-running text-danger'></i>Busy</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="row">
                     {
-                        this.props.monthNames.map(m => (
-                            <Month monthName={m} monthData={months[monthNames.indexOf(m)]} weekNames={this.props.weekNames} key={m}/>
+                        this.props.data.monthNames.map(m => (
+                            <Month monthName={m} monthData={months[this.props.data.monthNames.indexOf(m)]} weekNames={this.props.data.weekNames} key={m}/>
                         ))
                     }
                 </div>                
+            </div>
 
           )
     }
